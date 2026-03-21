929abf5860b16938abc49cd02e29ff7949c299ed
ennoreiji0 <ennoreiji0@gmail.com>
2026-03-21 14:14:49 +0900

    api of AI1

--- app/api/AI-api/route.ts
+++ app/api/AI-api/route.ts
@@ -1,10 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 
-let count=0
+
+export async function GET(request:NextRequest){
+    
+  return NextResponse.json({
+    
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { NextRequest, NextResponse } from "next/server";
+
+
 export async function GET(request:NextRequest){
     const {searchParams}=new URL(request.url)
     const name=searchParams.get('name') || 'ゲスト'
     return NextResponse.json({
         message:`okay!!!${name}!`,
     })
-}
+} */
--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     request:NextRequest,
-    {params}:{params:{id:string}}){
+    { params }: { params: Promise<{ id: string }> }){
     //const {searchParams}=new URL(request.url)
     const resolvedParams=await params;
     const userId=resolvedParams.id || 'gest';
