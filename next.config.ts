c50a805ab0b3e966048970ad7ca2100e5b25f224
ennoreiji0 <ennoreiji0@gmail.com>
2026-03-24 13:37:41 +0900

    test2

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* 静的エクスポートを有効にする */
-  output: 'export',
-  
-  /* 画像最適化を無効にする (Static Export時は必須) */
+  output: 'export', // これ
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
-
-  /* 任意：URLの末尾にスラッシュを入れる設定 (Cloudflareと相性がいい) */
   trailingSlash: true,
 };
 
